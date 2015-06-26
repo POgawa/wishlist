37ac99d6158ccff57027cce75469e88242348785
Paul Ogwa <student@epicodus.com>
2015-06-26 11:43:25 -0700

    Adds functioning book creation model and conditional if no reviews

--- app/controllers/book.js
+++ app/controllers/book.js
@@ -1,49 +1,52 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-//   needs: ['list'],
-//   actions: {
-//     // addBook: function() {
-//     //   this.get('model').forEach(function(book) {
-//     //     if (book.selected === true) {
-//     //       var book = this.store.createRecord('book', {
-//     //         isbn10: this.get('isbn10'),
-//     //         isbn13: this.get('isbn13'),
-//     //         title: this.get('title'),
-//     //         author: this.get('authors'),
-//     //         list: this.get('controllers.list.model')
-//     //       });
-//     //
-//     //       var list = this.get('controllers.list.model');
-//     //       book.save().then(function() {
-//     //         list.get('books').pushObject(book);
-//     //         list.save();
-//     //       });
-//     //
-//     //
-//     //       this.transitionToRoute('lists');
-//     //
-//     //
-//     //     }
-//     //   })
-//     // }
-//     addBook2: function() {
-//       var book = this.store.createRecord('book', {
-//         // isbn10: this.get('isbn10'),
-//         // isbn13: this.get('isbn13'),
-//         title: this.get('title'),
-//         author: this.get('authors'),
-//         list: this.get('controllers.list.model')
-//       });
-//
-//       var list = this.get('controllers.list.model');
-//       book.save().then(function() {
-//         list.get('books').pushObject(book);
-//         list.save();
-//       });
-//
-//
-//       this.transitionToRoute('lists');
-//     }
-//   }
+
+  needs: ['list'],
+  actions: {
+
+    //
+    // addBook: function() {
+
+    //   this.get('model').forEach(function(book) {
+    //     if (book.selected === true) {
+    //       var book = this.store.createRecord('book', {
+    //         isbn10: this.get('isbn10'),
+    //         isbn13: this.get('isbn13'),
+    //         title: this.get('title'),
+    //         author: this.get('authors'),
+    //         list: this.get('controllers.list.model')
+    //       });
+    //       var list = this.get('controllers.list.model');
+    //       book.save().then(function() {
+    //         list.get('books').pushObject(book);
+    //         list.save();
+    //       });
+    //
+    //
+    //       this.transitionToRoute('lists');
+    //
+    //
+    //     }
+    //   })
+    // }
+    addBook2: function(book) {
+      var book = this.store.createRecord('book', {
+        // isbn10: this.get('isbn10'),
+        // isbn13: this.get('isbn13'),
+        title: book.volumeInfo.title,
+        author: book.volumeInfo.authors.toString(),
+        list: this.get('controllers.list.model')
+      });
+
+      var list = this.get('controllers.list.model');
+      book.save().then(function() {
+        list.get('books').pushObject(book);
+        list.save();
+      });
+
+
+      this.transitionToRoute('lists');
+    }
+  }
 });
--- app/controllers/list.js
+++ app/controllers/list.js
@@ -12,5 +12,5 @@ export default Ember.Controller.extend({
         this.transitionToRoute('lists');
       }
     }
-  }  
+  }
 });
